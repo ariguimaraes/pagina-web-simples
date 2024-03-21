32e270132a14296bebf0ef7bee8be7084c3d59c5
Ari Guimarães <146673812+ariguimaraes@users.noreply.github.com>
2024-03-21 14:16:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,21 @@ function onChangePassword() {
 }
 
 function login() {
-    window.location.href = "pages/home/home.html";
+    console.log('antes')
+    firebase.auth().signInWithEmailAndPassword(
+        form.email().value, form.password().value
+    ).then(response => {
+        window.location.href = "pages/home/home.html";
+    }).catch(error => {
+        alert(getErrorMessage(error))
+    });
+}
+
+function getErrorMessage(error) {
+    if (error.code = "auth/user-not-found") {
+        return "Usuário não encontrado";
+    }
+    return error.message;
 }
 
 function register() {
